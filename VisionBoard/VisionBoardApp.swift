11836e6089d3a2ab4eded29a64da0ed186432d67
ghigbie <gehigbie@gmail.com>
2020-12-14 23:15:52 -0500

    Initial commit

---
+++ VisionBoard/VisionBoardApp.swift
@@ -0,0 +1,17 @@
+//
+//  VisionBoardApp.swift
+//  VisionBoard
+//
+//  Created by George Higbie on 12/14/20.
+//
+
+import SwiftUI
+
+@main
+struct VisionBoardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
