51e8da564fc8b33391bbe21107533c335e9fb0b8
ghigbie <gehigbie@gmail.com>
2020-12-15 21:52:21 -0500

    Add VSTACK and HSTACK

---
+++ README.md
@@ -0,0 +1,3 @@
+# Vision Board
+
+A basic app that serves as a vision board.
--- VisionBoard/ContentView.swift
+++ VisionBoard/ContentView.swift
@@ -9,13 +9,68 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
-    }
+        VStack(spacing: 12){
+            HStack(spacing: 12){
+                Image("tesla")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+                Image("husky")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+                Image("pool")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            } //HSTACK
+            
+            HStack(spacing: 12){
+                Image("tesla")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+                Image("husky")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+                Image("pool")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            } //HSTACK
+            
+            HStack(spacing: 12){
+                Image("tesla")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+                Image("husky")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+                
+                Image("pool")
+                    .resizable()
+                    .aspectRatio(contentMode: .fill)
+                    .frame(width: 100, height: 100)
+                    .cornerRadius(10)
+            } //HSTACK
+
+    }//VSTACK
+}
 }
 
-struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-    }
-}
